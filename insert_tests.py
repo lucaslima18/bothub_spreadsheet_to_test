950011083b2b5683390e5fb91d51e062f13b9724
lucaslima18 <lucas.ala1999@gmail.com>
2019-10-31 10:12:38 -0300

    creating te right and wornd sentences report in /csv directory

--- insert_tests.py
+++ insert_tests.py
@@ -1,21 +1,29 @@
 import csv
 from post import create_test
+from manage_files import right_sentences, wrong_sentences
 
 
 def test():
     print("responda as perguntas com sim ou nao!")
-    categorized = open('csv/susana.csv')
+    categorized = open('csv/categorized_messages.csv')
     mensagens = csv.DictReader(categorized)
+    right_sentences.write("text,intent,\n")
+    wrong_sentences.write("text,intent,\n")
+
     for mensagem in mensagens:
         text = mensagem['text']
         intent = mensagem['intent']
-        objeto = mensagem
 
-        test_question = input(f"A frase \"{text}\" faz parte da intenção \"{intent}\"?\n")
-        
-        if(test_question == 'sim'):
-            create_test(text, intent)
-        else:
-            intent = input("digite o nome da intenção:")
-            create_test(text, intent)
+        try:
+            test_question = input(f"A frase \"{text}\" faz parte da intenção \"{intent}\"?\n")
+            
+            if(test_question == 'sim'):
+                create_test(text, intent)
+                right_sentences.write(f"{text},{intent},%\n")
+            else:
+                intent = input("digite o nome da intenção:")
+                create_test(text, intent)
+                wrong_sentences.write(f"{text},{intent},%\n")
+        except:
+            print(f'caiu em exeção')
 
--- manage_files.py
+++ manage_files.py
@@ -1,10 +1,13 @@
 import csv, json
 
 #open csv files
-inicial_csv = open('csv/mensagens_susana.csv')
-categorized_messages = open('csv/susana.csv', 'w')
+inicial_csv = open('csv/inicial_csv.csv')
+categorized_messages = open('csv/categorized_messages.csv', 'w')
 exceptions = open('csv/exceptions.csv', 'w')
-report = open('csv/relatorios.md', 'w')
+report = open('csv/report.md', 'w')
+right_sentences = open('csv/right_sentences.csv', 'w')
+wrong_sentences = open('csv/wrong_sentences.csv', 'w')
+
 
 #open json files
 with open('json/intents.json') as j:
